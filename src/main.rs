49066322208848b38e580f3f5a92730fa63ba875
Neeraj Singh <neerajonetwo@gmail.com>
2024-02-03 20:44:42 +0530

    dep updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! hh");
+    println!();
+
+    let b = "Hello Rust";
+    println!("{}", b);
+
+    let calculate = cn();
+    println!("{}", calculate);
+}
+
+fn cn() -> i32 {
+    5 + 5
 }
