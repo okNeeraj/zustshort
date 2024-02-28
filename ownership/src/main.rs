cf795f0e18615fb6daa01f31731c833e4653d942
Neeraj Singh <neerajonetwo@gmail.com>
2024-02-28 20:34:43 +0530

    Borrowing in rust

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s1 = String::from("hello");
 
-    s.push_str(", world!"); // push_str() appends a literal to a String
+    let len = calculate_length(&s1);
 
-    println!("{}", s); // This will print `hello, world!`
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
